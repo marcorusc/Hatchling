cf678f9edcda8719a2d76d94543b5d62ec9e55a8
LittleCoinCoin <eliott.jacopin@laposte.net>
2025-05-27 11:12:45 +0900

    [Update] Hatchling package public API
    **Major**:
    - Giving access to `HatchMCP` so that people can properly build their Hatch-compliant servers
    - By the way, this means even the dummy test packages such as `base_pkg_1` needs a small update
    - It might also be removed in the future depending how we manage to support inclusion of third-party MCP servers that would be relying on the vanilla FastMCP.

--- hatchling/__init__.py
+++ hatchling/__init__.py
@@ -5,5 +5,11 @@ This package provides a CLI interface for interacting with LLMs
 with MCP Tool Calling capabilities.
 """
 
-from importlib.metadata import version
-__version__ = version("hatchling")
+# from importlib.metadata import version
+# __version__ = version("hatchling")
+
+from hatchling.mcp_utils.hatch_mcp import HatchMCP
+
+__all__ = [
+    'HatchMCP',
+]
